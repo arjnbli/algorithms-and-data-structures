f441fe835eb0afcd6f25c0f8dc42adefa50f4aae
Arjun <37790028+arjnbli@users.noreply.github.com>
2022-02-18 15:18:55 -0700

    add leetcode 49

---
+++ leetcode/lc49.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        pass
+
+
+if __name__ == '__main__':
+   solution = Solution()
+   print(solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+   print(solution.groupAnagrams([""]))
+   print(solution.groupAnagrams(["a"]))
+   
