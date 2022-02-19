c07259989fcaf0df6155d4e372031fcb023c1970
Arjun <37790028+arjnbli@users.noreply.github.com>
2022-02-18 18:29:33 -0700

    add leetcode 93

---
+++ leetcode/lc93.py
@@ -0,0 +1,9 @@
+class Solution:
+    def restoreIpAddresses(self, s: str):
+        pass
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(Solution.restoreIpAddresses('25525511135'))
+    print(Solution.restoreIpAddresses('0000'))
+    print(Solution.restoreIpAddresses('101023'))
