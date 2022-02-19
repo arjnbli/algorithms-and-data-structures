ce299bb16b435332cf67add3f3def4f65fcca5f7
Arjun <37790028+arjnbli@users.noreply.github.com>
2022-02-18 17:35:45 -0700

    solve lc49

--- leetcode/lc49.py
+++ leetcode/lc49.py
@@ -1,6 +1,21 @@
+from signal import strsignal
+
+
 class Solution:
     def groupAnagrams(self, strs):
-        pass
+        #n - number of words
+        #w - length of longest word
+        #O(n*w*log(w)) time | O(n*w) space
+        anagrams = {}
+        for word in strs:
+            #O(w*log(w) + w + w)
+            sortedWord = ''.join(sorted(word)) 
+            #O(1)
+            if sortedWord not in anagrams:
+                anagrams[sortedWord] = [word]
+            else:
+                anagrams[sortedWord].append(word)
+        return list(anagrams.values())
 
 
 if __name__ == '__main__':
